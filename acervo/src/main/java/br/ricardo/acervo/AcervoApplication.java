4dd0afe3cb1a0d05613d20b73d3cd020958b3000
Ricardo Andre Lopes Ikeda <89215809+RicardoALIkeda@users.noreply.github.com>
2025-03-24 11:27:18 +0000

    ss

--- acervo/src/main/java/br/ricardo/acervo/AcervoApplication.java
+++ acervo/src/main/java/br/ricardo/acervo/AcervoApplication.java
@@ -1,13 +1,18 @@
 package ricardo.acervo;
 
+import br.seu_nome.acervo.aplicacao.ConsoleApp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
-public class AcervoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(AcervoApplication.class, args);
-	}
-
+public class AcervoApplication implements CommandLineRunner {
+ public static void main(String[] args) {
+ SpringApplication.run(AcervoApplication.class, args);
+ }
+ @Override
+ public void run(String... args) {
+ ConsoleApp consoleApp;
+ consoleApp.iniciar();
+ }
 }
